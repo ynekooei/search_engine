75a1e7361bb41375ec7e679ab31bd99d98cb4177
Henrythecoder <luisnajera1610@gmail.com>
2021-01-29 15:08:21 -0800

    Update scraper.py
    added way to parse text and tokenize the words into a list

--- spacetime-crawler4py-master/scraper.py
+++ spacetime-crawler4py-master/scraper.py
@@ -2,17 +2,44 @@ import re
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 #import tokenizer
-
+from nltk.tokenize import RegexpTokenizer
+import nltk
+nltk.download('stopwords')
 
 
 def scraper(url, resp):
+    #check for HTML status code:
+    #  we can only check current url since we have current response and status code for that url only
+        # less than 200 -> ERROR1
+        # 200 to 399(inclusive) -> OK
+        # 400+ -> ERROR2
     links = extract_next_links(url, resp)
     result =[]
+    soup2 = BeautifulSoup(resp.raw_response.content, 'html5lib')
+    # get title 
+    text = soup2.title.get_text()
+    
+    for x in soup2.find_all('p'):
+        text += x.get_text()
+    
+    tokenizer = RegexpTokenizer('\w+')
+    tokens = tokenizer.tokenize(text)
+    valid_words = []
+    stopwords = nltk.corpus.stopwords.words('english')
+
+    for word in tokens:
+        if word not in stopwords:
+            valid_words.append(word.lower())
+    
+    # PUT valid words length and current url stats in website stat file
+
+
+    
     for link in links:
         if is_valid(link):
-            #figure out how to extract 'valuable' text
-            #soup2 = BeautifulSoup(resp.raw_response.content, 'html5lib')
-            #text = soup2.find_all("p")
+            
+            
+            
             #print(text)
             #tokenize the words (call tokenize function from assignment1)
             result.append(link)
@@ -22,7 +49,7 @@ def scraper(url, resp):
 def extract_next_links(url, resp):
     # Implementation requred.
     list = []
-    #print(resp.raw_response.text[0:500])
+    
     if resp.raw_response is None:
         return list
     soup = BeautifulSoup(resp.raw_response.content, 'html5lib')
@@ -44,10 +71,7 @@ def is_valid(url):
         if parsed.scheme not in set(["http", "https"]):
             return False
         
-        #check for HTML status code:
-        # less than 200 -> ERROR1
-        # 200 to 399(inclusive) -> OK
-        # 400+ -> ERROR2
+        
         
 
 
