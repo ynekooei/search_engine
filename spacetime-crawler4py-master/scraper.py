73958c5d069b775c158fb7cc3bf1165ee6cf1fca
Henrythecoder <luisnajera1610@gmail.com>
2021-02-01 02:38:08 -0800

    Update scraper.py
    added functions for pickle to save url word count and word frequencies. Scraper function now removes url fragments

--- spacetime-crawler4py-master/scraper.py
+++ spacetime-crawler4py-master/scraper.py
@@ -8,18 +8,21 @@ nltk.download('stopwords')
 from collections import defaultdict
 import pickle
 
-def scraper(url, resp):
-    #  we can only check current url since we have current response and status code for that url only
 
+def scraper(url, resp):
+    #check for HTML status code:
+    
     links = extract_next_links(url, resp)
     result =[]
-    
-    if ((resp.status < 200) or (resp.status > 399)):
+
+    if ((resp.status < 200) or (resp.status > 399) or (resp.raw_response is None) or (resp.raw_response.content == "")):
         return result
-    
+
     soup2 = BeautifulSoup(resp.raw_response.content, 'html5lib')
-    # get title 
-    text = soup2.title.get_text()
+    # get title
+    text = ""
+    if soup2.title != None: 
+        text = soup2.title.get_text()
     
     for x in soup2.find_all('p'):
         text += x.get_text()
@@ -36,13 +39,29 @@ def scraper(url, resp):
     # compute hash (simhash) using valid_words and check againsts previous hashes for page similarity
     # if the same then return empty list otherwise, proceed        
     
-    # put valid words length and current url stats in website stat file
+    # put valid words length and current url stats in website stat dictionary
+    url_word_length = defaultdict(int)
+    try:
+        with open('stats_word_count.pickle', 'rb') as handle:
+            url_word_length = pickle.load(handle)
+    except (OSError, IOError) as e:
+       pickle.dump(url_word_length, open('stats_word_count.pickle', "wb"))
+
+    url_word_length[url] = len(valid_words)
+    
+    with open('stats_word_count.pickle', 'wb') as handle:
+        pickle.dump(url_word_length, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
     
+
+
     word_frequency = defaultdict(int)
     #load word frequencies using pickle
-    #with open('stats_word_frequency', 'rb') as handle:
-        #word_frequency = pickle.load(handle)
+    try:
+        with open('stats_word_frequency.pickle', 'rb') as handle:
+            word_frequency = pickle.load(handle)
+    except (OSError, IOError) as e:
+        pickle.dump(word_frequency, open('stats_word_frequency.pickle', "wb"))
 
     #update word_frequency file 
     for word in valid_words: 
@@ -51,21 +70,23 @@ def scraper(url, resp):
 
     
     #store word_frequency into file
-    #with open('stats_word_frequency', 'wb') as handle:
-        #pickle.dump(word_frequency, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('stats_word_frequency.pickle', 'wb') as handle:
+        pickle.dump(word_frequency, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     #with open('stats_word_frequency', 'rb') as handle:
         #word_frequency = pickle.load(handle)
 
+
     
     for link in links:
         if is_valid(link):
-            
+            # remove link fragments
+            new_link= link.split('#')[0]
             
             
             #print(text)
             #tokenize the words (call tokenize function from assignment1)
-            result.append(link)
+            result.append(new_link)
     return result
     
 
@@ -104,6 +125,10 @@ def is_valid(url):
             or (re.match(r"^today\.uci\.edu$", parsed.netloc) and 
                 re.match(r"\/department\/information_computer_sciences(\/.+|$)", parsed.path)))):
             return False
+
+
+        if (re.match(r"^evoke.ics.uci.edu", parsed.netloc) and re.search(r"replytocom=[a-zA-Z0-9]+(\/$|$)", parsed.query)):
+            return False    
         
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
